9a9a2d11e525e42a154c81cbd258f4aa90526113
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-07-14 14:52:28 +0530

    [Niketan]Add: Welcome to Day-20 Mood Analyser Problems

---
+++ MoodAnalyserProblem/MoodAnalyserProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day-20 Mood Analyser Problems ");
+            
+            Console.ReadLine();
+        }
+    }
+}
